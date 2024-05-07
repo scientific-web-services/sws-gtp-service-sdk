b90f96a101ef5777621a692a9f3ef67424d971e1
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2024-05-07 08:20:59 -0700

    Add HIP-RA

--- src/sws_gtp_service_sdk/__init__.py
+++ src/sws_gtp_service_sdk/__init__.py
@@ -8,6 +8,7 @@ from requests.adapters import HTTPAdapter
 from requests.adapters import Retry
 
 
+
 class GeophiresSimulationParameters:
     def __init__(self):
         self._parameters = {}
@@ -46,8 +47,32 @@ class GeophiresSimulationResult:
         self.simulation_result = simulation_result
 
 
+class HipRaParameters:
+    def __init__(self):
+        self._parameters = {}
+
+    def get_parameters(self) -> dict:
+        return self._parameters
+
+    def with_parameter(self, parameter_name: str, parameter_value: Any):  # -> Self:
+        self._parameters[parameter_name] = parameter_value
+        return self
+
+
+class HipRaRequest:
+    def __init__(self, parameters: HipRaParameters):
+        self._parameters = parameters
+
+    def get_hip_ra_parameters(self) -> HipRaParameters:
+        return self._parameters
+
+
+class HipRaResult:
+    def __init__(self, result: dict):
+        self.hip_ra_result: dict = result
+
 class GtpServiceClient:
-    def __init__(self, endpoint: str, api_key:str = None):
+    def __init__(self, endpoint: str, api_key: str = None):
         self._endpoint = endpoint
         self._session = requests.Session()
 
@@ -73,3 +98,13 @@ class GtpServiceClient:
             headers={'x-api-key': self._api_key} if self._api_key is not None else None
         )
         return GeophiresSimulationResult(json.loads(response.text))
+
+    def get_hip_ra_result(self, hip_ra_request: HipRaRequest):
+        response = self._session.post(
+            f'{self._endpoint}/get-hip-ra-result',
+            json={
+                'hip_ra_input_parameters': hip_ra_request.get_hip_ra_parameters().get_parameters()},
+            timeout=30,
+            headers={'x-api-key': self._api_key} if self._api_key is not None else None
+        )
+        return HipRaResult(json.loads(response.text))
