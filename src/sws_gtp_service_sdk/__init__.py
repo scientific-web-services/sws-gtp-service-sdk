6550e9ff4f54fe54ca76c388393a4fdcbe868a26
sws-jonathan <jonathan@scientificwebservices.com>
2024-05-24 16:27:55 -0700

    Update, describe, share result

--- src/sws_gtp_service_sdk/__init__.py
+++ src/sws_gtp_service_sdk/__init__.py
@@ -1,6 +1,7 @@
 __version__ = '0.7.1'
 
 import json
+from enum import Enum
 from typing import Any
 
 import requests
@@ -50,6 +51,11 @@ class GeophiresResult:
     def result_id(self) -> str:
         return self.geophires_result['ResultId']
 
+
+class DescribeGeophiresResultsRequest:
+    def __init__(self, result_id: str = None):
+        self.result_id = result_id
+
 class DescribeGeophiresResultsResult:
     def __init__(self, geophires_results:list[GeophiresResult]):
         self.geophires_results = geophires_results
@@ -59,6 +65,23 @@ class DeleteGeophiresResultRequest:
         self.result_id = result_id
 
 class DeleteGeophiresResultResult:
+    def __init__(self, result_id: str):
+        self.result_id = result_id
+
+
+class SharePolicy(str, Enum):
+    PUBLIC = '*'
+    PRIVATE = ''
+
+
+class UpdateGeophiresResultRequest:
+    def __init__(self, result_id: str = None, description: str = None, share_policy: SharePolicy = None):
+        self.result_id = result_id
+        self.description = description
+        self.share_policy = share_policy
+
+
+class UpdateGeophiresResultResult:
     def __init__(self, result_id: str):
         self.result_id = result_id
 
@@ -105,15 +128,14 @@ class GtpServiceClient:
 
         # TODO should probably enable closing session
 
-    def get_geophires_result(self, geophires_request: GeophiresRequest):
-        # -> GeophiresResult:
+    def get_geophires_result(self, geophires_request: GeophiresRequest) -> GeophiresResult:
 
         response = self._session.post(
             f'{self._endpoint}/get-geophires-result',
             json={
                 'geophires_input_parameters': geophires_request.get_geophires_parameters().get_parameters()},
             timeout=30,
-            headers=self._get_geophires_service_headers()
+            headers=self._get_api_key_headers()
         )
         response_dict:dict = json.loads(response.text)
 
@@ -128,7 +150,7 @@ class GtpServiceClient:
             json={
                 'hip_ra_input_parameters': hip_ra_request.get_hip_ra_parameters().get_parameters()},
             timeout=30,
-            headers=self._get_geophires_service_headers()
+            headers=self._get_api_key_headers()
         )
         return HipRaResult(json.loads(response.text))
 
@@ -138,21 +160,26 @@ class GtpServiceClient:
             json={
                 'geophires_input_parameters': geophires_request.get_geophires_parameters().get_parameters()},
             timeout=30,
-            headers=self._get_gtp_service_headers()
+            headers=self._get_auth_token_headers()
         )
         response_dict: dict = json.loads(response.text)
 
         if 'message' in response_dict and response_dict['message'] == 'Endpoint request timed out':
             raise requests.Timeout(response_dict['message'])
 
-        # FIXME TODO
         return GeophiresResult(response_dict)
 
-    def describe_geophires_results(self) -> DescribeGeophiresResultsResult:
+    def describe_geophires_results(self,
+                                   request: DescribeGeophiresResultsRequest = None) -> DescribeGeophiresResultsResult:
+        filters = {}
+        if request is not None and request.result_id is not None:
+            filters['result_id'] = request.result_id
+
         response = self._session.post(
             f'{self._endpoint}/describe-geophires-results',
             timeout=30,
-            headers=self._get_gtp_service_headers()
+            json=filters,
+            headers=self._get_auth_token_headers()
         )
         response_list: list[dict[str,Any]] = json.loads(response.text)
 
@@ -161,6 +188,42 @@ class GtpServiceClient:
 
         return DescribeGeophiresResultsResult([GeophiresResult(entry) for entry in response_list])
 
+    def get_shared_geophires_result(self, result_id) -> GeophiresResult:
+        response = self._session.post(
+            f'{self._endpoint}/get-shared-geophires-result',
+            timeout=30,
+            json={'result_id': result_id},
+            headers=self._get_api_key_headers()
+        )
+        response_dict: dict = json.loads(response.text)
+
+        if 'message' in response_dict and response_dict['message'] == 'Endpoint request timed out':
+            raise requests.Timeout(response_dict['message'])
+
+        return GeophiresResult(response_dict)
+
+    def update_geophires_result(self, request: UpdateGeophiresResultRequest) -> UpdateGeophiresResultResult:
+        update_json = {
+            'result_id': request.result_id,
+        }
+
+        if request.description is not None:
+            update_json['description'] = request.description
+
+        if request.share_policy is not None:
+            update_json['share_policy'] = request.share_policy.value
+
+        response = self._session.post(
+            f'{self._endpoint}/update-geophires-result',
+            json=update_json,
+            timeout=30,
+            headers=self._get_auth_token_headers()
+        )
+        response_dict: dict = json.loads(response.text)
+
+        return UpdateGeophiresResultResult(response_dict['ResultId'])
+
+
     def delete_geophires_result(self, request:DeleteGeophiresResultRequest) -> DeleteGeophiresResultResult:
         response = self._session.post(
             f'{self._endpoint}/delete-geophires-result',
@@ -168,20 +231,20 @@ class GtpServiceClient:
                 'result_id': request.result_id
             },
             timeout=30,
-            headers=self._get_gtp_service_headers()
+            headers=self._get_auth_token_headers()
         )
         response_dict: dict = json.loads(response.text)
 
         return DeleteGeophiresResultResult(response_dict['ResultId'])
 
-    def _get_geophires_service_headers(self):
+    def _get_api_key_headers(self):
         headers = {}
         if self._api_key is not None:
             headers['x-api-key'] = self._api_key
 
         return headers
 
-    def _get_gtp_service_headers(self):
+    def _get_auth_token_headers(self):
         headers = {}
         if self._auth_token is not None:
             headers['Authorization'] = f'Bearer {self._auth_token}'
