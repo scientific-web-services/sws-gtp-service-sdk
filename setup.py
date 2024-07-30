b7a9d92d1af73c2c7045a7911c1624e9ed1a474b
sws-jonathan <jonathan@scientificwebservices.com>
2024-07-30 10:40:50 -0700

    Bump version: 0.7.2 → 0.8.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name='sws-gtp-service-sdk',
-    version='0.7.2',
+    version='0.8.0',
     license='MIT',
     description='SWS GTP service SDK',
     long_description='{}\n{}'.format(
